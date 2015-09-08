3fa82d7d5c655b1278646829ed0a4b1fa90eb45d
martin.pitt <martin.pitt@lab49.com>
2015-09-08 14:21:26 +0100

    Log and recover when dispensary is down

--- boxtape-cli/src/main/java/io/boxtape/cli/core/resolution/dispensary/DispensaryPlayResolver.kt
+++ boxtape-cli/src/main/java/io/boxtape/cli/core/resolution/dispensary/DispensaryPlayResolver.kt
@@ -2,44 +2,57 @@ package io.boxtape.cli.core.resolution.dispensary
 
 import com.google.common.collect.ArrayListMultimap
 import com.mashape.unirest.http.Unirest
+import com.mashape.unirest.http.exceptions.UnirestException
 import io.boxtape.asJson
+import io.boxtape.cli.Loggers
 import io.boxtape.cli.core.resolution.PlayRepository
 import io.boxtape.cli.core.resolution.PlayResolver
 import io.boxtape.core.LibraryArtifact
 import io.boxtape.core.ansible.PlayProvider
-import io.boxtape.mappers.Mappers
 import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.util.Optional
 
 @Component
 public class DispensaryPlayResolver
-@Autowired constructor(val config: DispenaryConfig, val repository:PlayRepository) : PlayResolver {
+@Autowired constructor(val config: DispenaryConfig, val repository: PlayRepository) : PlayResolver {
 
     val log = LogFactory.getLog(this.javaClass)
+    val consoleLog = Loggers.BOXTAPE_CONSOLE
 
     override fun resolve(dependencies: Iterable<LibraryArtifact>): ArrayListMultimap<LibraryArtifact, PlayProvider> {
         val request = DispensaryLookupRequest(
             requirements = dependencies.map { it.name() }
         )
         log.info("Requesting to dispensary at ${config.url} : \n ${request.asJson()}")
-        val response = Unirest.post("${config.url}/lookup")
-            .header("Content-Type","application/json")
-            .body(request)
-//            .asJson()
+        val resultSet = getLookupResult(request)
+        val providersByArtifact: ArrayListMultimap<LibraryArtifact, PlayProvider> = ArrayListMultimap.create()
+        if (resultSet.isPresent()) {
+            val downloaded = repository.downloadMissing(resultSet.get().matches)
 
-            .asObject(javaClass<DispensaryLookupResultSet>())
-
-//        val json = response.getBody()
-        val resultSet = response.getBody() // Mappers.jsonMapper.convertValue(json, javaClass<DispensaryLookupResultSet>() )
-        val downloaded = repository.downloadMissing(resultSet.matches)
-        val providersByArtifact:ArrayListMultimap<LibraryArtifact,PlayProvider> = ArrayListMultimap.create()
-        downloaded.entries()
-        downloaded.asMap().forEach { entry ->
-            val libraryArtifact = dependencies.first { it.name().equals(entry.getKey()) }
-            providersByArtifact.putAll(libraryArtifact, entry.getValue())
+            downloaded.entries()
+            downloaded.asMap().forEach { entry ->
+                val libraryArtifact = dependencies.first { it.name().equals(entry.getKey()) }
+                providersByArtifact.putAll(libraryArtifact, entry.getValue())
+            }
         }
         return providersByArtifact
+    }
+
+    private fun getLookupResult(request: DispensaryLookupRequest): Optional<DispensaryLookupResultSet> {
+        try {
+            val response = Unirest.post("${config.url}/lookup")
+                .header("Content-Type", "application/json")
+                .body(request)
+                .asObject(javaClass<DispensaryLookupResultSet>())
+            val resultSet = response.getBody() // Mappers.jsonMapper.convertValue(json, javaClass<DispensaryLookupResultSet>() )
+            return Optional.of(resultSet)
+        } catch (e: UnirestException) {
+            consoleLog.warn("It looks like the dispensary at ${config.url} is down.")
+            consoleLog.warn("Attempts to connect returned the following error: ${e.getMessage()}")
+            return Optional.empty()
+        }
     }
 }
 
